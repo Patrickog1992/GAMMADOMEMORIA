7ec79edb76ae04d0ae4708e4b210db513f07a237
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-09-05 18:00:13 +0000

    ATENÇÃO: Devido as grande indústrias que lucram com remédios para memóri

--- src/components/alert-banner.tsx
+++ src/components/alert-banner.tsx
@@ -4,9 +4,8 @@ export default function AlertBanner() {
   return (
     <div className="bg-destructive text-destructive-foreground text-center p-3 font-semibold text-base md:text-lg sticky top-0 z-50">
       <p>
-        ATENÇÃO: Devido a NASA saber desse projeto o site irá estar no ar
-        somente até hoje:{" "}
-        <span className="text-yellow-400">05/09/2025</span> :(
+        ATENÇÃO: Devido as grande indústrias que lucram com remédios para memória estar nos processando esse site irá sair do ar hoje:{" "}
+        <span className="text-yellow-400">05/09/2025</span>
       </p>
     </div>
   );
