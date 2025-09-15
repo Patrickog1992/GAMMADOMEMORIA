8db21e6b0d0ac688b0bebd437050394e0b8b4a11
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-09-15 18:47:35 +0000

    troque o video por esse
    <vturb-smartplayer id="vid-68c85d6f84ed93da140

--- src/components/video-player.tsx
+++ src/components/video-player.tsx
@@ -7,11 +7,11 @@ export default function VideoPlayer() {
     <>
       <div className="w-full max-w-[400px] mx-auto my-8">
         <vturb-smartplayer
-          id="vid-68bb1e4e28c13a747f0847e6"
+          id="vid-68c85d6f84ed93da140df9cd"
         ></vturb-smartplayer>
       </div>
       <Script
-        src="https://scripts.converteai.net/1a6a90ad-f1f6-4f11-b6be-e02b59de709c/players/68bb1e4e28c13a747f0847e6/v4/player.js"
+        src="https://scripts.converteai.net/b45e4a12-72fd-43f2-a7e4-73d6b242d5d9/players/68c85d6f84ed93da140df9cd/v4/player.js"
         strategy="lazyOnload"
       />
     </>
