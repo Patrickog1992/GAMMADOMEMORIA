e8d2845c20cedfc716f3c5c0cd92a795d3c30183
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-09-18 20:15:46 +0000

    troque o pixel desse site por esse
    <!-- Meta Pixel Code -->
    <script>
    !

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1414601842942499');
+            fbq('init', '4214249325486870');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -40,7 +40,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1414601842942499&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=4214249325486870&ev=PageView&noscript=1"
           />
         </noscript>
         {children}
